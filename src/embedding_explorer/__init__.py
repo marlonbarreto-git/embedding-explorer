bc6cc2bf4b7716a8ac0188e83d6f09184c545824
Marlon Barreto <mbarretot@hotmail.com>
2026-02-12 15:54:13 -0500

    chore: add py.typed marker and __all__ exports

--- src/embedding_explorer/__init__.py
+++ src/embedding_explorer/__init__.py
@@ -1,3 +1,14 @@
 """Embedding Explorer - Visualize and compare text embeddings across models."""
 
+__all__ = [
+    "Embedder",
+    "EmbeddingResult",
+    "Point2D",
+    "SimilarityPair",
+    "Visualizer",
+]
+
 __version__ = "0.1.0"
+
+from .embedder import Embedder, EmbeddingResult
+from .visualizer import Point2D, SimilarityPair, Visualizer
